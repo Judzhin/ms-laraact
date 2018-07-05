0c9308ca5191d376a4b02f506b38e65a7a521919
Judzhin Miles <info@msbios.com>
2018-07-05 23:23:44 +0300

    Init Example

---
+++ app/Product.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @access protected
+ * @author Judzhin Miles <info[woof-woof]msbios.com>
+ */
+namespace Laraact;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * Class Product
+ * @package Laraact
+ */
+class Product extends Model
+{
+    // ...
+}
